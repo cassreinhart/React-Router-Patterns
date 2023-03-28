1ddec13c5a2456899f739b27a76e502674dfaeec
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-03-28 18:00:08 -0500

    set up dog-finder basic structure

--- dog-finder/src/App.js
+++ dog-finder/src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import whiskey from './whiskey.jpg';
+import duke from './duke.jpg'
+import perry from './perry.jpg'
+import tubby from './tubby.jpg'
+import { Route, Switch, Redirect } from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <Switch>
+        <Route exact path='/dogs'>
+          <DogList />
+        </Route>
+        <Route exact path='/dogs/:name'>
+          <DogDetails />
+        </Route>
+        <Redirect to="/dogs" />
+      </ Switch>
     </div>
   );
 }
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: "Whiskey",
+      age: 5,
+      src: whiskey,
+      facts: [
+        "Whiskey loves eating popcorn.",
+        "Whiskey is a terrible guard dog.",
+        "Whiskey wants to cuddle with you!"
+      ]
+    },
+    {
+      name: "Duke",
+      age: 3,
+      src: duke,
+      facts: [
+        "Duke believes that ball is life.",
+        "Duke likes snow.",
+        "Duke enjoys pawing other dogs."
+      ]
+    },
+    {
+      name: "Perry",
+      age: 4,
+      src: perry,
+      facts: [
+        "Perry loves all humans.",
+        "Perry demolishes all snacks.",
+        "Perry hates the rain."
+      ]
+    },
+    {
+      name: "Tubby",
+      age: 4,
+      src: tubby,
+      facts: [
+        "Tubby is really stupid.",
+        "Tubby does not like walks.",
+        "Angelina used to hate Tubby, but claims not to anymore."
+      ]
+    }
+  ]
+};
 
 export default App;
